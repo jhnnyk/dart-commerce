07f68e537ff1e8d96b73da1e38c1084cefaf8f9e
John Knotts <jhnnyk@gmail.com>
2024-09-05 06:20:32 -0600

    adding the interactive prompt

--- checkout.dart
+++ checkout.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 class Product {
   const Product({required this.id, required this.name, required this.price});
   final int id;
   final String name;
   final double price;
+
+  String get displayName => '($initial)${name.substring(1)}: \$$price';
+  String get initial => name.substring(0, 1);
 }
 
 class Item {
@@ -15,4 +20,47 @@ class Item {
 
 class Cart {
   // TODO: Items Collection
+}
+
+const allProducts = [
+  Product(id: 1, name: 'apples', price: 1.60),
+  Product(id: 2, name: 'bananas', price: 0.70),
+  Product(id: 3, name: 'courgettes', price: 1.0),
+  Product(id: 4, name: 'grapes', price: 2.00),
+  Product(id: 5, name: 'mushrooms', price: 0.80),
+  Product(id: 6, name: 'potatoes', price: 1.50),
+];
+
+void main() {
+  while (true) {
+    stdout.write(
+        'What do you want to do? (v)iew items, (a)dd item, (c)heckout: ');
+    final line = stdin.readLineSync();
+
+    if (line == 'a') {
+      final product = chooseProduct();
+      if (product != null) {
+        print(product.displayName);
+      }
+    } else if (line == 'v') {
+      // TODO
+    } else if (line == 'c') {
+      // TODO
+    }
+  }
+}
+
+Product? chooseProduct() {
+  final productsList =
+      allProducts.map((product) => product.displayName).join('\n');
+  stdout.write('Available products:\n$productsList\nYour choice: ');
+
+  final line = stdin.readLineSync();
+  for (var product in allProducts) {
+    if (product.initial == line) {
+      return product;
+    }
+  }
+  print('Not found');
+  return null;
 }
