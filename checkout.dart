9dc23ff2a2fa41651989fd73354f4aff9a1b7d6d
John Knotts <jhnnyk@gmail.com>
2024-09-05 05:56:15 -0600

    creating Product and Item classes

---
+++ checkout.dart
@@ -0,0 +1,18 @@
+class Product {
+  const Product({required this.id, required this.name, required this.price});
+  final int id;
+  final String name;
+  final double price;
+}
+
+class Item {
+  const Item({required this.product, this.quantity = 1});
+  final Product product;
+  final int quantity;
+
+  double get price => product.price * quantity;
+}
+
+class Cart {
+  // TODO: Items Collection
+}
